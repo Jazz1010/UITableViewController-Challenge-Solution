bd464089923bbef0eadf41ecd47f4e0a6f02c1e1
Maddock <maddock1010@gmail.com>
2015-03-06 15:34:37 +0800

    Initial Commit

---
+++ UITableViewController Challenge Solution/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  UITableViewController Challenge Solution
+//
+//  Created by Maddock on 2015/3/6.
+//  Copyright (c) 2015年 Maddock. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ UITableViewController Challenge Solution/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  UITableViewController Challenge Solution
+//
+//  Created by Maddock on 2015/3/6.
+//  Copyright (c) 2015年 Maddock. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
